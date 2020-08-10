dbb81f0420d5ad460f121210cc553d749472e0be
Douglas Santiago <douglas.rodrigues@ivoryit.com.br>
2020-08-10 17:13:50 -0300

    Implementando o design do item de transferência

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: null,
+        ),
+        body: Card(
+          child: Text('Aoba'),
+        ),
+      ),
+    ),
+  );
+}
